2fc3f394839ae99280b4de1b8c9d1b6329686bab
Igor Zalutski <izalutski@gmail.com>
2024-03-12 16:25:17 +0000

    Create main.tf

---
+++ c/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_integer" "example" {
+  min = 1
+  max = 100
+}
+
+output "random_integer" {
+  value = random_integer.example.result
+}
