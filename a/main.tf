b055c22ab6b82c0a729a7d79722a2c977e383692
Igor Zalutski <izalutski@gmail.com>
2024-03-12 17:27:06 +0000

    Add dynamic randomness

--- a/main.tf
+++ a/main.tf
@@ -17,3 +17,11 @@ resource "random_integer" "example" {
 output "random_integer" {
   value = random_integer.example.result
 }
+
+data "external" "random" {
+  program = ["${path.module}/random.sh"]
+}
+
+output "random_value_dynamic" {
+  value = data.external.random.result["random_value"]
+}
---
+++ a/random.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "{\"random_value\":\"$(openssl rand -hex 5)\"}"
